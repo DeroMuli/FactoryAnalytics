97043efbb0d0a8424c38046c41edd536020e3587
DeroMuli <musembimuli1999@gmail.com>
2022-11-03 15:57:18 +0300

    synchronise phone theme with app theme

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
-import React, { useState } from 'react'
-import { NavigationContainer , DarkTheme, DefaultTheme } from '@react-navigation/native';
+import React, { useCallback, useEffect, useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native';
 import MainNavigator from './src/navigation/MainNavigator';
 import {
   MD3DarkTheme as PaperDarkTheme,
   Provider as PaperProvider,
-  MD3LightTheme as PaperLightTheme
+  MD3LightTheme as PaperLightTheme,
+  MD3Theme
 } from 'react-native-paper';
-import { Appearance } from 'react-native';
+import { Appearance, ColorSchemeName } from 'react-native';
+import {useColorScheme} from 'react-native'
 
 export default function App() : JSX.Element {
-  const [theme,settheme] = useState(PaperDarkTheme)
+  const [theme, setTheme] = useColorScheme() == "dark" ? useState<MD3Theme>(PaperDarkTheme) : useState<MD3Theme>(PaperLightTheme);
+  console.log(theme.dark)
   return (
     <PaperProvider theme={theme}>
       <NavigationContainer>
