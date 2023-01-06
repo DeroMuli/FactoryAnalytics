0eefd8839f487a0b7dd88ce1ecbaac5d6d5040e2
DeroMuli <musembimuli1999@gmail.com>
2023-01-06 11:45:17 +0300

    added a patch to react native to use react-native-svg-charts with depreceated ViewPropTypes

--- src/screens/EquipmentScreen.tsx
+++ src/screens/EquipmentScreen.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import { screen_names } from '../constants/ScreenNames';
 import { ParamListBase } from '@react-navigation/native';
+import { ProgressBar, MD3Colors } from 'react-native-paper';
+import { useTheme } from "react-native-paper";
+import { ProgressCircle } from 'react-native-svg-charts'
 import {
   StyleSheet,
   SafeAreaView,
@@ -9,9 +12,11 @@ import {
 } from "react-native";
 
 const EquipmentScreen = ({navigation}: NativeStackScreenProps<ParamListBase, screen_names.EQUIPMENT, undefined>) => {
+  const {colors} = useTheme()
     return (
       <SafeAreaView style={styles.container}>
-        <Text>general data and basic control</Text>
+        <ProgressBar progress={0.5} style={{width : 300,height : 8 , marginTop : 10, borderRadius : 5 , transform: [{ scaleX: 5 }, { scaleY: 1 }]}}  color={MD3Colors.primary40} />
+        <Text>Some</Text>
       </SafeAreaView>
   )
   };
