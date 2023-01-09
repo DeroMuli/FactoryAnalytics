1aefb58f220073e7472e6b7d09a14f4ed1430b30
DeroMuli <musembimuli1999@gmail.com>
2023-01-09 15:37:55 +0300

    finished up the speedcontrolcomponent

--- src/components/SpeedControlComponent.tsx
+++ src/components/SpeedControlComponent.tsx
@@ -3,6 +3,7 @@ import definedcolor from '../constants/colors';
 import { useTheme } from "react-native-paper";
 import { ProgressCircle } from 'react-native-svg-charts'
 import {Slider} from '@miblanchard/react-native-slider';
+import { Switch } from 'react-native-paper';
 import {
   StyleSheet,
   Text
@@ -11,7 +12,13 @@ import {
 const SpeedControlComponent = () => {
     const {fonts} = useTheme()
     const [speed,setspeed] = useState<number>(0)
-
+    const [isSwitchOn, setIsSwitchOn] = useState(true);
+    const onToggleSwitch = () => {
+        setIsSwitchOn(!isSwitchOn)
+        if(isSwitchOn){
+            setspeed(0)
+        }
+    }
     const speedchanged = (value : number | number[]) => {
       let num = value[0]
       setspeed(num)  
@@ -28,7 +35,18 @@ const SpeedControlComponent = () => {
         cornerRadius={0}
       />
     <Text style={{fontWeight : fonts.displayLarge.fontWeight , fontFamily : fonts.displayLarge.fontFamily , fontSize : fonts.displayLarge.fontSize , top : -130}}>{Math.trunc(speed)}</Text>
-    <Slider step={1} onValueChange={speedchanged} thumbTintColor={definedcolor.progresscircle} minimumTrackTintColor={definedcolor.progresscircle} minimumValue={0} maximumValue={100} containerStyle={{width : 150,height : 10  ,top : -130}}/>
+    <Slider 
+    step={1} 
+    onValueChange={speedchanged} 
+    thumbTintColor={definedcolor.progresscircle} 
+    minimumTrackTintColor={definedcolor.progresscircle} 
+    minimumValue={0} 
+    maximumValue={100} 
+    containerStyle={{width : 150,height : 10  ,top : -130}}
+    value={speed}
+    disabled={!isSwitchOn}
+    />
+    <Switch color={definedcolor.progresscircle} style={{top : -130 , marginTop : 5}} value={isSwitchOn} onValueChange={onToggleSwitch} />
     </>
     )
 }
