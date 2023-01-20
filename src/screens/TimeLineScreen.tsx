0f47f275782609bbc89a2b91e7da17e4b563ff7e
DeroMuli <musembimuli1999@gmail.com>
2023-01-20 12:18:29 +0300

    timeline screen rough draft

--- src/screens/TimeLineScreen.tsx
+++ src/screens/TimeLineScreen.tsx
@@ -1,18 +1,73 @@
 import React from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Text } from "react-native";
+import { Text, View, FlatList, StyleProp, ViewStyle } from "react-native";
 import { screen_names } from "../constants/ScreenNames";
 import { ParamListBase } from "@react-navigation/native";
 import { SafeAreaView } from "react-native";
+import colors from "../constants/colors";
+import FancySeparator from "../assets/icons/FancySeparator";
+
+//events card mock data
+const DATA = [
+  {
+    id: "1",
+  },
+  {
+    id: "2",
+  },
+  {
+    id: "3",
+  },
+  {
+    id: "4",
+  },
+  {
+    id: "5",
+  },
+  {
+    id: "6",
+  },
+  {
+    id: "7",
+  },
+];
 
 const TimeLineScreen = ({
   navigation,
 }: NativeStackScreenProps<ParamListBase, screen_names.TIMELINE, undefined>) => {
+  const renderItem = ({ item }) => (
+    <View
+      style={{
+        height: 101,
+        width: 300,
+        backgroundColor: "#FFFFFF",
+        borderRadius: 10,
+      }}
+    ></View>
+  );
   return (
-    <SafeAreaView
-      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-    >
-      <Text>Feed Screen</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <FlatList
+        contentContainerStyle={{
+          padding: 10,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent={(item) => {
+          const separatorcolor =
+            item.leadingItem.id % 2 == 0 ? "#7DE1C3" : "#FF6E4D";
+          const separatorstyle: StyleProp<ViewStyle> =
+            item.leadingItem.id % 2 == 0
+              ? { marginStart: 280 }
+              : { marginStart: 20 };
+          return (
+            <FancySeparator color={separatorcolor} style={separatorstyle} />
+          );
+        }}
+      />
     </SafeAreaView>
   );
 };
