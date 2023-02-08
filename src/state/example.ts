5859c96630564ba7b7bcfe15e4a6e29cf9c74e71
DeroMuli <musembimuli1999@gmail.com>
2023-02-08 10:33:43 +0300

    added type safety to slices

--- src/state/example.ts
+++ src/state/example.ts
@@ -1,11 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "./store";
 
 //example of using rtk querry ... to be deleted later
+interface counterState {
+  value: number;
+}
+
+const initialState: counterState = {
+  value: 0,
+};
 export const counterSlice = createSlice({
   name: "counter",
-  initialState: {
-    value: 0,
-  },
+  initialState,
   reducers: {
     increment: (state) => {
       // Redux Toolkit allows us to write "mutating" logic in reducers. It
@@ -17,7 +23,8 @@ export const counterSlice = createSlice({
     decrement: (state) => {
       state.value -= 1;
     },
-    incrementByAmount: (state, action) => {
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    incrementByAmount: (state, action: PayloadAction<number>) => {
       state.value += action.payload;
     },
   },
@@ -25,5 +32,8 @@ export const counterSlice = createSlice({
 
 // Action creators are generated for each case reducer function
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectCount = (state: RootState) => state.counter.value;
 
 export default counterSlice.reducer;
