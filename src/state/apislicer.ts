17e7442d226c678e21382b8dedd582d81509d467
DeroMuli <musembimuli1999@gmail.com>
2023-02-10 10:28:23 +0300

    stored cache for 24 hours

--- src/state/apislicer.ts
+++ src/state/apislicer.ts
@@ -2,11 +2,14 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { REST_URL } from "@env";
 import type { fetcheddata_schema } from "../screens/HomeScreen";
 
+const day_in_seconds = 60 * 60 * 24;
+
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: REST_URL }),
   endpoints: (builder) => ({
     getFactoryEquipmentsData: builder.query<Array<fetcheddata_schema>, void>({
       query: () => "",
+      keepUnusedDataFor: day_in_seconds,
     }),
   }),
 });
