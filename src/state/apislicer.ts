e7ec13d1d409d4eea9bb2cea89e76690e5cba083
DeroMuli <musembimuli1999@gmail.com>
2023-02-10 12:31:55 +0300

    ensure no caching to prevent 304 error when reloading expo

--- src/state/apislicer.ts
+++ src/state/apislicer.ts
@@ -8,7 +8,15 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: REST_URL }),
   endpoints: (builder) => ({
     getFactoryEquipmentsData: builder.query<Array<fetcheddata_schema>, void>({
-      query: () => "",
+      query: () => ({
+        url: "",
+        method: "GET",
+        prepareHeaders: (headers) => {
+          headers.set("Content-Type", "application/json");
+          headers.set("Cache-Control", "no-store");
+          return headers;
+        },
+      }),
       keepUnusedDataFor: day_in_seconds,
     }),
   }),
