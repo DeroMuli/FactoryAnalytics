c255c6110cc6a2362e21820c5a8f377e90cef391
DeroMuli <musembimuli1999@gmail.com>
2023-03-23 14:19:12 +0300

    [refactor] : removed the analytics screen

--- src/navigation/BottomTabsNavigator.tsx
+++ src/navigation/BottomTabsNavigator.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { screen_names } from "../constants/ScreenNames";
 import AboutScreen from "../screens/AboutScreen";
-import AnalyticsScreen from "../screens/AnalyticsScreen";
 import TimeLineScreen from "../screens/TimeLineScreen";
 import HomeScreen from "../screens/HomeScreen";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
-import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import { useTheme } from "react-native-paper";
 
@@ -54,26 +52,6 @@ const BottomTabNavigator = (): JSX.Element => {
                   : theme.colors.inversePrimary
               }
               name="clock-o"
-              size={20}
-            />
-          ),
-          headerTitleAlign: "center",
-          headerStyle: { backgroundColor: theme.colors.elevation.level0 },
-          headerTitleStyle: { color: theme.colors.onPrimary },
-        }}
-      />
-      <Tab.Screen
-        component={AnalyticsScreen}
-        name={screen_names.ANALYTICS}
-        options={{
-          tabBarIcon: (props: tabBarIconsProps) => (
-            <MaterialIcons
-              color={
-                props.focused
-                  ? theme.colors.primary
-                  : theme.colors.inversePrimary
-              }
-              name="analytics"
               size={20}
             />
           ),
