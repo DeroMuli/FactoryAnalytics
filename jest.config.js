bc1eecd76acf92e6f92dcdffce614d181ec32a35
DeroMuli <musembimuli1999@gmail.com>
2023-07-16 17:44:28 +0300

    tests with babel-jest and react-test-renderer .... ( VectorIcons tests still failing )

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+//no clue this works without adding any transform specifically for tsx/ts files
+module.exports = {
+  preset: "react-native",
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.jsx$": "babel-jest",
+  },
+  testMatch: ["**/?(*.)+(spec|test).ts?(x)"],
+};
--- src/components/Heading.test.tsx
+++ src/components/Heading.test.tsx
@@ -1,6 +1,11 @@
 import Heading from "./Headings";
 import renderer from "react-test-renderer";
+import { Text } from "react-native";
 
 describe("Heading", () => {
-  it("renders correctly", () => {});
+  it("renders correctly", () => {
+    const heading = "Heading";
+    const tree = renderer.create(<Heading heading={heading} />);
+    expect(tree.root.findByType(Text).props.children).toBe(heading);
+  });
 });
