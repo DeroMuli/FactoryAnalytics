81e1b48522fc78315f57d451c98ec7db1df17bf0
DeroMuli <musembimuli1999@gmail.com>
2023-07-25 14:53:05 +0300

    added some basic tests for the factory equipment card

--- src/components/Cards/FactoryEquipmentCard/FactoryEquipmentCard.test.tsx
+++ src/components/Cards/FactoryEquipmentCard/FactoryEquipmentCard.test.tsx
@@ -8,6 +8,8 @@ import {} from "@testing-library/jest-native";
 import { Provider } from "react-redux";
 import { store } from "../../../state/store";
 import type { Icon } from "../../../assets/icons/VectorIcons";
+import { setEquipements } from "../../../state/equipmentstateslicer";
+import { NavigationContainer } from "@react-navigation/native";
 
 describe("FactoryEquipmentCard", () => {
   const name = "Test Machine";
@@ -15,21 +17,37 @@ describe("FactoryEquipmentCard", () => {
   const mean_speed = 20;
   const mean_temp = 30;
   const socket = new WebSocket("ws://localhost:8080");
+  store.dispatch(setEquipements([{ name, isOn: false }]));
   const TestFactoryCardEquipment = (
-    <Provider store={store}>
-      <PaperProvider theme={PaperDarkTheme}>
-        <FactoryEquipmentCard
-          name={name}
-          icon={icon}
-          mean_speed={mean_speed}
-          mean_temp={mean_temp}
-          socket={socket}
-        />
-      </PaperProvider>
-    </Provider>
+    <FactoryEquipmentCard
+      name={name}
+      icon={icon}
+      mean_speed={mean_speed}
+      mean_temp={mean_temp}
+      socket={socket}
+    />
   );
   it("should render the FactoryEquipmentCard component", () => {
-    render(TestFactoryCardEquipment);
+    render(TestFactoryCardEquipment, { wrapper: ProviderWrapper });
     expect(screen.getByTestId("FactoryEquipmentCard")).toBeTruthy();
   });
+  it("display the name of the equipment", () => {
+    render(TestFactoryCardEquipment, { wrapper: ProviderWrapper });
+    expect(screen.getByText(name)).toBeVisible();
+  });
+  it("displays the mean spead and mean temp", () => {
+    render(TestFactoryCardEquipment, { wrapper: ProviderWrapper });
+    expect(screen.getByText(`${mean_speed}° C`)).toBeVisible();
+    expect(screen.getByText(`${mean_temp}m/s`)).toBeVisible();
+  });
 });
+
+const ProviderWrapper = ({ children }: any) => {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <PaperProvider theme={PaperDarkTheme}>{children}</PaperProvider>
+      </NavigationContainer>
+    </Provider>
+  );
+};
