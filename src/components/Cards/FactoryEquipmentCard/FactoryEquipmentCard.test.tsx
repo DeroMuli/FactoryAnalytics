54769335a1a089388bff90415c7e9fa6d3f5eeb5
DeroMuli <musembimuli1999@gmail.com>
2023-07-28 15:59:49 +0300

    fifnished the factoryequipmentcard tests

--- src/components/Cards/FactoryEquipmentCard/FactoryEquipmentCard.test.tsx
+++ src/components/Cards/FactoryEquipmentCard/FactoryEquipmentCard.test.tsx
@@ -2,22 +2,47 @@ import FactoryEquipmentCard from "./FactoryEquipmentCard";
 import {
   MD3DarkTheme as PaperDarkTheme,
   Provider as PaperProvider,
+  Switch,
 } from "react-native-paper";
-import { render, screen } from "@testing-library/react-native";
+import { fireEvent, render, screen } from "@testing-library/react-native";
 import {} from "@testing-library/jest-native";
 import { Provider } from "react-redux";
 import { store } from "../../../state/store";
 import type { Icon } from "../../../assets/icons/VectorIcons";
 import { setEquipements } from "../../../state/equipmentstateslicer";
 import { NavigationContainer } from "@react-navigation/native";
+import { Server, Client } from "mock-socket";
+
+const name = "Test Machine";
+const mockurl = "ws://localhost:8080";
+
+//push the assert code to the correct test blocks or find a way to clearly describe what is being tested
+jest.mock("@react-navigation/native", () => {
+  const actualNav = jest.requireActual("@react-navigation/native");
+  return {
+    ...actualNav,
+    useNavigation: jest.fn(() => ({
+      navigate: jest.fn((name: String, params: { Equipemt_name: String }) => {
+        expect(name).toBe("Equipment");
+        expect(params.Equipemt_name).toBe("Test Machine");
+      }),
+    })),
+  };
+});
+
+beforeEach(() => {
+  store.dispatch(setEquipements([{ name, isOn: false }]));
+});
+
+afterEach(() => {
+  store.dispatch(setEquipements([]));
+});
 
 describe("FactoryEquipmentCard", () => {
-  const name = "Test Machine";
   const icon: Icon = { iconlibrary: "MaterialCommunityIcons", iconname: "fan" };
   const mean_speed = 20;
   const mean_temp = 30;
-  const socket = new WebSocket("ws://localhost:8080");
-  store.dispatch(setEquipements([{ name, isOn: false }]));
+  const socket = new WebSocket(mockurl);
   const TestFactoryCardEquipment = (
     <FactoryEquipmentCard
       name={name}
@@ -39,6 +64,34 @@ describe("FactoryEquipmentCard", () => {
     render(TestFactoryCardEquipment, { wrapper: ProviderWrapper });
     expect(screen.getByText(`${mean_speed}° C`)).toBeVisible();
     expect(screen.getByText(`${mean_temp}m/s`)).toBeVisible();
+  });
+  it("has the right background color when the switch is off and changes when the switch on", () => {
+    render(TestFactoryCardEquipment, { wrapper: ProviderWrapper });
+    expect(screen.getByTestId("FactoryEquipmentCard")).toHaveStyle({
+      backgroundColor: PaperDarkTheme.colors.onBackground,
+    });
+    //use ally labels to find the switch when you add accessibilityLabel to the switch
+    fireEvent(screen.UNSAFE_getByType(Switch), "onValueChange");
+    expect(screen.getByTestId("FactoryEquipmentCard")).toHaveStyle({
+      backgroundColor: PaperDarkTheme.colors.onPrimaryContainer,
+    });
+  });
+  it("chages the state and sends the data to the web socket server when the switch is pressed", () => {
+    let mockServer = new Server(mockurl);
+    mockServer.on("connection", (socket: Client) => {
+      socket.on("message", (data: string) => {
+        expect(data).toBe(JSON.stringify({ action: "toggle", payload: "ON" }));
+      });
+    });
+    render(TestFactoryCardEquipment, { wrapper: ProviderWrapper });
+    expect(store.getState().equipment[0].isOn).toBe(false);
+    //use ally labels to find the switch when you add accessibilityLabel to the switch
+    fireEvent(screen.UNSAFE_getByType(Switch), "onValueChange");
+    expect(store.getState().equipment[0].isOn).toBe(true);
+  });
+  it("navigates to the equipment screen when the card is pressed", () => {
+    render(TestFactoryCardEquipment, { wrapper: ProviderWrapper });
+    fireEvent.press(screen.getByTestId("FactoryEquipmentCard"));
   });
 });
 
