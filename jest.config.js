acbee3d72e7105ba1a123ffc0dee25f86fcde6c6
DeroMuli <musembimuli1999@gmail.com>
2023-08-03 17:34:57 +0300

    transpiled a bunch of node modules to avoid mocking vector-icons used in VectorIcons

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,8 @@ module.exports = {
   preset: "jest-expo",
   testMatch: ["**/?(*.)+(spec|test).ts?(x)"],
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
-  setupFiles: ["./jest.setup.js"],
   testEnvironment: "jsdom",
   transformIgnorePatterns: [
-    "node_modules/(?!(@miblanchard|@react-native|react-native|react-native-svg-charts)/)",
+    "node_modules/(?!(@miblanchard|@react-native|react-native|react-native-svg-charts|@expo/vector-icons|expo-font|expo-modules-core|expo-asset|expo-constants)/)",
   ],
 };
